08bc5a7d26c4a745a0a9d6639cba7131155d5c3a
ds5105119 <ds5105119@gmail.com>
2025-06-11 01:10:10 +0900

    Adds terms agreement display
    프로젝트 생성 흐름의 첫 번째 단계에서 필요한 약관 동의 사항을 표시합니다. 사용자가 프로젝트를 생성하기 전에 필요한 약관에 동의하도록 안내합니다.

--- src/components/section/service/project/new/createproject.tsx
+++ src/components/section/service/project/new/createproject.tsx
@@ -17,6 +17,7 @@ import CreateProjectFormStep1 from "./createprojectstep1";
 import CreateProjectFormStep2 from "./createprojectstep2";
 import CreateProjectFormStep3 from "./createprojectstep3";
 import CreateProjectSide from "./createprojectside";
+import { CheckIcon } from "lucide-react";
 
 export default function CreateProject({ session }: { session: Session | null }) {
   const router = useRouter();
@@ -380,6 +381,31 @@ export default function CreateProject({ session }: { session: Session | null }) 
                 {currentStep === 3 && <CreateProjectFormStep3 form={form} />}
               </form>
             </Form>
+
+            {currentStep === 1 && (
+              <div className="flex flex-col w-full h-full mt-4">
+                <div className="w-full">
+                  <p className="text-sm font-medium text-muted-foreground mb-4">프로젝트를 만들기 위해 동의가 필요한 약관들이에요</p>
+                </div>
+                <div className="flex flex-col w-full space-y-1.5">
+                  <div className="w-full flex space-x-1.5">
+                    <CheckIcon className="mt-[0.25rem] !size-5 text-blue-500" strokeWidth={3} />
+                    <div className="space-x-1">
+                      <span className="text-sm font-bold text-blue-500">필수</span>
+                      <span className="text-sm font-medium">Fellows 서비스 약관 및 개인정보 수집 · 이용 · 제공 동의</span>
+                    </div>
+                  </div>
+
+                  <div className="w-full flex space-x-2">
+                    <CheckIcon className="mt-[0.25rem] !size-5 text-blue-500" strokeWidth={3} />
+                    <div className="space-x-1">
+                      <span className="text-sm font-bold text-blue-500">필수</span>
+                      <span className="text-sm font-medium">개인정보 제 3자 제공 동의</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         ) : (
           <div className="w-full h-full px-5 sm:px-8 py-6 md:py-10 flex flex-col items-center">
