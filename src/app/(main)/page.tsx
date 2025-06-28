d94a026e9084d93280eb95225551c5ed75b7495d
ds5105119 <ds5105119@gmail.com>
2025-06-29 01:53:58 +0900

    메인 섹션에 블러 효과 제거
    메인 페이지의 첫 번째 섹션에서 블러 효과를 제거합니다.
    
    이는 디자인 변경 사항을 반영하고, 블러 효과가 시각적으로 최적의 결과를 제공하지 않는다고 판단했기 때문입니다.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -8,9 +8,9 @@ import { BlurFade } from "@/components/magicui/blur-fade";
 export default async function Home() {
   return (
     <div className="grid grid-cols-4 lg:grid-cols-12 mb-4 md:mb-24">
-      <BlurFade className="col-span-full pt-24 md:pt-38 pb-8 px-4 lg:px-16">
+      <div className="col-span-full pt-24 md:pt-38 pb-8 px-4 lg:px-16">
         <MainSection1 />
-      </BlurFade>
+      </div>
       <BlurFade className="col-span-full py-8 lg:py-16 px-4 lg:px-16 w-full max-w-[1400px] lg:mx-auto">
         <MainSection2 />
       </BlurFade>
