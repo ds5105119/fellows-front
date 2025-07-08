fe4437f12aa16b9c45b23c2d314ec150baf8a47e
ds5105119 <ds5105119@gmail.com>
2025-07-09 01:38:22 +0900

    제거: 사용되지 않는 아이콘 제거
    사용되지 않는 `UserIcon`을 제거하여 컴포넌트의 의존성을 정리하고 코드베이스를 간소화합니다.

--- src/components/section/service/project/main/teams-list.tsx
+++ src/components/section/service/project/main/teams-list.tsx
@@ -8,7 +8,7 @@ import { Badge } from "@/components/ui/badge";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { EllipsisVertical, Info, Trash2, UserIcon, UserPlus } from "lucide-react";
+import { EllipsisVertical, Info, Trash2, UserPlus } from "lucide-react";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import {
   DropdownMenu,
