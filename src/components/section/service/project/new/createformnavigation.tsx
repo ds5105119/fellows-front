85570bd6aac8ae5fa9ecdb3961867fff7ae3843b
ds5105119 <ds5105119@gmail.com>
2025-08-04 01:37:34 +0900

    수정: 뒤로 가기 버튼 표시 조건 업데이트
    프로젝트 생성 폼에서 첫 번째 단계에서는 뒤로 가기 버튼이 표시되지 않도록 조건을 수정합니다.

--- src/components/section/service/project/new/createformnavigation.tsx
+++ src/components/section/service/project/new/createformnavigation.tsx
@@ -34,7 +34,7 @@ export function ProjectFormNavigation({
       <div className="w-full h-4 bg-gradient-to-t from-background to-transparent" />
 
       <div className="w-full flex justify-between space-x-4 pb-4 pt-3 bg-background">
-        {currentStep > 0 && (
+        {currentStep > 1 && (
           <Button
             type="button"
             className="flex-1 w-1/2 h-[3.5rem] rounded-2xl text-base md:text-lg font-semibold"
