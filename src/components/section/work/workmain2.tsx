e6f01bbaf9ec6f231d4511002e4ed691ee991a83
ds5105119 <ds5105119@gmail.com>
2025-08-06 13:03:39 +0900

    제거합니다: 불필요한 백슬래시
    오류로 인해 생성된 불필요한 백슬래시를 제거합니다. 이는 코드의 가독성을 향상시킵니다.

--- src/components/section/work/workmain2.tsx
+++ src/components/section/work/workmain2.tsx
@@ -6,7 +6,6 @@ import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 export default function WorkMain2() {
   return (
     <div className="relative w-full flex flex-col mt-[calc(96vh+4px)] md:mt-[calc(96vh+4px)]">
-      \{" "}
       <div className="mx-4 hidden md:block">
         <div className="relative w-full aspect-[23/9] overflow-hidden">
           <GridDistortion imageSrc="/workmain.avif" mouse={0.25} strength={0.15} className="object-cover" />
