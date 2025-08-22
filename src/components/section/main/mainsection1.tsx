a7286f482907f6e54d94d9271f7dd406817e2672
김동현 <52624315+ds5105119@users.noreply.github.com>
2025-08-22 10:52:58 +0000

    Adds introductory text to main section
    Adds text introducing the service and its benefits to the main section of the landing page.
    
    This text highlights cost savings and quality maintenance by leveraging global outsourcing.

--- src/components/section/main/mainsection1.tsx
+++ src/components/section/main/mainsection1.tsx
@@ -15,13 +15,20 @@ export default async function MainSection1() {
       <div className="flex flex-col gap-8 items-center justify-center w-full h-full">
         <div className="flex w-full flex-col z-20 items-center justify-center rounded-2xl">
           <div className="w-full pt-6 md:pt-2 flex flex-col gap-2 md:gap-3 items-center justify-center">
-            <MainSection1Design />
             <div className="w-full flex flex-col md:flex-row gap-1 md:gap-2.5 items-center justify-center text-foreground text-center">
               <h1 className="text-2xl xl:text-5xl font-bold tracking-normal">Web, App 개발</h1>
               <h1 className="text-2xl xl:text-5xl font-bold tracking-normal">Fellows℠에 문의하세요</h1>
             </div>
 
-            {/* 클라이언트 컴포넌트에 session과 쿠키에서 읽은 초기값을 전달합니다. */}
+            <div className="w-full flex flex-col items-center justify-center text-foreground text-center pt-1 md:pt-4.5">
+              <div className="w-full flex flex-col md:flex-row items-center justify-center gap-0.5 md:gap-1">
+                <h3 className="text-sm xl:text-lg font-semibold tracking-normal text-muted-foreground w-fit">글로벌 외주 인력을 통해</h3>
+                <h3 className="text-sm xl:text-lg font-semibold tracking-normal text-muted-foreground w-fit">최소 30%의 비용을 절감해보세요.</h3>
+              </div>
+              <h3 className="text-sm xl:text-lg font-semibold tracking-normal text-muted-foreground hidden md:block w-full">
+                개발 비용은 더 낮아지고 높은 퀄리티는 유지할 수 있어요.
+              </h3>
+            </div>
             <MainSection1Form session={session} initialDescription={decodeURIComponent(pendingDescription)} />
           </div>
         </div>
