f80df015ced097b2207b144714ffddc2a5ccde5f
김동현 <52624315+ds5105119@users.noreply.github.com>
2025-08-26 06:02:02 +0000

    robots.txt 및 언어 설정을 업데이트합니다
    robots.txt를 업데이트하여 불필요한 크롤링을 방지하고, 사이트맵 URL을 HTTPS로 변경합니다.
    
    또한, HTML 언어 설정을 한국어로 변경하여 검색 엔진 최적화를 개선합니다.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -69,7 +69,7 @@ export default async function RootLayout({
   };
 
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="ko_KR" className="scroll-smooth">
       <Analytics />
       <SpeedInsights />
       <GoogleAnalytics />
