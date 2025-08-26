845cf7a624bdff5e359e4c474a7b181c52372078
김동현 <52624315+ds5105119@users.noreply.github.com>
2025-08-26 13:55:34 +0000

    제거: 불필요한 WorkMain1 컴포넌트를 제거합니다
    WorkMain1 컴포넌트를 제거하여 페이지 레이아웃을 단순화하고 유지 관리성을 향상시킵니다.
    
    더 이상 사용되지 않는 컴포넌트를 제거하여 코드베이스를 정리합니다.

--- src/app/(work)/works/page.tsx
+++ src/app/(work)/works/page.tsx
@@ -1,11 +1,9 @@
-import WorkMain1 from "@/components/section/work/workmain1";
 import WorkMain2 from "@/components/section/work/workmain2";
 import WorkMain3 from "@/components/section/work/workmain3";
 
 export default async function Home() {
   return (
     <div className="grid grid-cols-4 lg:grid-cols-12">
-      <WorkMain1 />
       <div className="col-span-full w-full">
         <WorkMain2 />
       </div>
