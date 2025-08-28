dca12f1b8deb1ab5321dd5db216acfe0ec03a16d
김동현 <52624315+ds5105119@users.noreply.github.com>
2025-08-28 04:01:21 +0000

    커서 상호작용을 추가하고 UI를 개선합니다
    연락처 섹션에 커서 상호작용을 추가하여 사용자 경험을 향상시키고, 반응형 디자인을 개선하여 다양한 화면 크기에서 UI가 일관되게 표시되도록 합니다. 또한 이메일 링크에 커서를 추가하여 사용자가 링크를 쉽게 식별하고 클릭할 수 있도록 합니다.

--- src/components/section/main/maincontactsection.tsx
+++ src/components/section/main/maincontactsection.tsx
@@ -10,6 +10,7 @@ import UnderlineToBackground from "@/components/fancy/text/underline-to-backgrou
 import { Button } from "@/components/ui/button";
 import { RefreshCwIcon } from "lucide-react";
 import { useInView } from "framer-motion";
+import { Cursor } from "@/components/resource/cursor";
 
 const socialLinks = [
   {
@@ -18,30 +19,42 @@ const socialLinks = [
     y: "10%",
     children: (
       <>
-        <div className="w-1/3 p-8 md:p-14 text-base md:text-xl font-medium text-[#0015ff] leading-loose">CONTACT</div>
-        <div className="w-2/3 p-8 md:p-14 text-base md:text-xl font-medium text-[#0015ff] leading-loose">
-          <Link href="mailto:sales@iihus.com">
-            <UnderlineToBackground targetTextColor="#f0f0f0" className="cursor-pointer">
+        <div className="w-1/3 p-8 xl:p-14 text-base lg:text-lg xl:text-xl font-medium text-[#0015ff] leading-loose">CONTACT</div>
+        <div className="w-2/3 p-8 xl:p-14 text-base lg:text-lg xl:text-xl font-medium text-[#0015ff] leading-loose">
+          <Link href="mailto:contact@iihus.com" className="relative !cursor-none">
+            <Cursor attachToParent>
+              <div className="text-2xl">👆</div>
+            </Cursor>
+            <UnderlineToBackground targetTextColor="#f0f0f0" className="!cursor-none">
+              CONTACT@IIHUS.COM
+            </UnderlineToBackground>
+          </Link>
+          <br />
+          <Link href="mailto:sales@iihus.com" className="relative !cursor-none">
+            <Cursor attachToParent>
+              <div className="text-2xl">👆</div>
+            </Cursor>
+            <UnderlineToBackground targetTextColor="#f0f0f0" className="!cursor-none">
               SALES@IIHUS.COM
             </UnderlineToBackground>
           </Link>
           <br />
-          <Link href="mailto:support@iihus.com">
-            <UnderlineToBackground targetTextColor="#f0f0f0" className="cursor-pointer">
+          <Link href="mailto:support@iihus.com" className="relative !cursor-none">
+            <Cursor attachToParent>
+              <div className="text-2xl">👆</div>
+            </Cursor>
+            <UnderlineToBackground targetTextColor="#f0f0f0" className="!cursor-none">
               SUPPORT@IIHUS.COM
             </UnderlineToBackground>
           </Link>
           <br />
-          <Link href="mailto:iih@iihus.com">
-            <UnderlineToBackground targetTextColor="#f0f0f0" className="cursor-pointer">
-              IIH@IIHUS.COM
-            </UnderlineToBackground>
-          </Link>
           <br />
-          <br />
-          INSTAGRAM&nbsp;
-          <Link href="https://instagram.com/fellows.my" target="_blank" rel="noopener noreferrer">
-            <UnderlineToBackground targetTextColor="#f0f0f0" className="cursor-pointer">
+          IG&nbsp;
+          <Link href="https://instagram.com/fellows.my" target="_blank" rel="noopener noreferrer" className="relative !cursor-none">
+            <Cursor attachToParent>
+              <div className="text-2xl">👆</div>
+            </Cursor>
+            <UnderlineToBackground targetTextColor="#f0f0f0" className="!cursor-none">
               @FELLOWS.MY
             </UnderlineToBackground>
           </Link>
@@ -146,15 +159,15 @@ export default function MainContactSection() {
   }, [inView]);
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
       <div className="col-span-1 flex flex-col gap-4 md:gap-6 aspect-square">
         <div className="w-full h-fit flex">
-          <div className="w-2/7 rounded-3xl md:rounded-4xl p-8 md:p-14 bg-[#45f3a2] flex items-center justify-center">
+          <div className="w-2/7 rounded-3xl md:rounded-4xl p-8 xl:p-14 bg-[#45f3a2] flex items-center justify-center">
             <AspectRatio ratio={1 / 1}>
               <Image alt="펠로우즈 이미지 로고" src="/fellows/logo-img.svg" fill className="obejct-cover" />
             </AspectRatio>
           </div>
-          <div className="w-5/7 rounded-3xl md:rounded-4xl p-8 md:p-14 bg-[#45f3a2] flex items-center justify-center">
+          <div className="w-5/7 rounded-3xl md:rounded-4xl p-8 xl:p-14 bg-[#45f3a2] flex items-center justify-center">
             <AspectRatio ratio={139 / 31}>
               <Image alt="펠로우즈 글자 로고" src="/fellows/logo-text.svg" fill className="obejct-cover" />
             </AspectRatio>
@@ -162,7 +175,7 @@ export default function MainContactSection() {
         </div>
         <div className="w-full h-full flex-1 bg-zinc-100 rounded-3xl md:rounded-4xl flex">{socialLinks.filter((l) => l.name == selectedMenu)[0]?.children}</div>
       </div>
-      <div ref={containerRef} className="relative col-span-1 bg-zinc-100 rounded-3xl md:rounded-4xl aspect-square overflow-hidden">
+      <div ref={containerRef} className="relative col-span-1 bg-zinc-100 rounded-3xl md:rounded-4xl flex-1 overflow-hidden">
         <PhysicsGravitySection key={key} onMenuSelect={setSelectedMenu} />
         <div className="absolute right-5 top-5">
           <Button variant="ghost" size="icon" className="focus-visible:ring-0 rounded-full bg-zinc-800 hover:bg-zinc-700" onClick={handleClick}>
