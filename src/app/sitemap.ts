9e5a59246b6e67cf6e4d0c5f528e3ca817adb794
김동현 <52624315+ds5105119@users.noreply.github.com>
2025-08-31 14:07:09 +0000

    사이트맵 생성 방식 개선
    기존의 정적 사이트맵 생성 방식에서 동적으로 블로그 게시물을 가져와 사이트맵에 추가하는 방식으로 변경합니다.
    
    이를 통해 사이트맵이 항상 최신 상태를 유지하고, 새로운 게시물이 추가될 때마다 자동으로 사이트맵에 반영되도록 합니다.
    
    사이트맵 생성 방식 개선
    
    정적 사이트맵 생성 방식에서 동적으로 블로그 게시물을 가져와 사이트맵을 생성하도록 변경합니다.
    
    이를 통해 사이트맵이 항상 최신 상태를 유지하고, 새로운 게시물이 추가될 때마다 자동으로 사이트맵에 반영됩니다.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,24 +1,51 @@
+import { BlogPostDto } from "@/@types/service/blog";
 import type { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://fellows.my",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 1,
-    },
-    {
-      url: "https://www.fellows.my",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 1,
-    },
-    {
-      url: "https://fellows.my/blog",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
-    },
-  ];
+const defaultSiteMap: MetadataRoute.Sitemap = [
+  {
+    url: "https://fellows.my",
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 1,
+  },
+  {
+    url: "https://www.fellows.my",
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 1,
+  },
+  {
+    url: "https://fellows.my/blog",
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 0.9,
+  },
+];
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BLOG_URL}/path`);
+  const posts = await res.json();
+
+  const sitemapFromPosts = await Promise.all(
+    posts.map(async (id: string) => {
+      const response = await fetch(`https://api.fellows.my/api/blog/${id}`, {
+        next: { revalidate: 60 },
+      });
+
+      if (!response.ok) return null;
+
+      const post_json = await response.json();
+      const post = BlogPostDto.parse(post_json);
+
+      return {
+        url: `https://fellows.my/blog/${id}`,
+        lastModified: post.published_at ?? undefined,
+        changeFrequency: "daily",
+        priority: 0.7,
+        images: [post.title_image],
+      };
+    })
+  );
+
+  return [...defaultSiteMap, ...sitemapFromPosts.filter(Boolean)];
 }
