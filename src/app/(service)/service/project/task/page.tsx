e1b3258779fbafed0858b6a62dad1538538fdfc3
김동현 <52624315+ds5105119@users.noreply.github.com>
2025-09-04 13:59:03 +0000

    suspense관련 업데이트

--- src/app/(service)/service/project/task/page.tsx
+++ src/app/(service)/service/project/task/page.tsx
@@ -6,22 +6,18 @@ import { GanttChart } from "@/components/section/service/task/gantt-chart";
 import { TreeTable } from "@/components/section/service/task/tree-table";
 import TaskNavigation from "@/components/section/service/task/task-navigation";
 
-function Task() {
+export default function Page() {
   const [taskView, setTaskView] = useState(false);
   const searchParams = useSearchParams();
 
   const projectId = searchParams.getAll("project_id");
 
   return (
-    <div className="shrink-0 w-full h-full flex flex-col">
-      <TaskNavigation taskView={taskView} setTaskView={setTaskView} />
-      {taskView ? <TreeTable /> : <GanttChart initalProjectId={projectId} />}
-    </div>
+    <Suspense>
+      <div className="shrink-0 w-full h-full flex flex-col">
+        <TaskNavigation taskView={taskView} setTaskView={setTaskView} />
+        {taskView ? <TreeTable /> : <GanttChart initalProjectId={projectId} />}
+      </div>
+    </Suspense>
   );
-}
-
-export default function Page() {
-  <Suspense>
-    <Task />
-  </Suspense>;
 }
