8532981ada7af7a03f582eb2b82a301bd64332ac
김동현 <52624315+ds5105119@users.noreply.github.com>
2025-09-15 13:30:33 +0000

    텍스트를 업데이트합니다
    메인 섹션의 텍스트를 업데이트하여 제공되는 개발 서비스의 범위를 명확히 합니다.

--- src/components/section/main/mainsection1.tsx
+++ src/components/section/main/mainsection1.tsx
@@ -29,7 +29,7 @@ export default async function MainSection1() {
               </h2>
 
               <h4 className="scroll-m-20 text-lg font-semibold leading-normal text-muted ml-1 md:mt-1.5">
-                어차피 써야하는 개발비라면 스마트하게 AI견적부터 테스크 추적까지,
+                쇼피파이, 아임웹, React, 앱 개발까지
                 <br />
                 최소 30% 더 적은 비용으로 외주를 진행하세요.<span className="text-[#e64646] font-black">*</span>
               </h4>
@@ -57,7 +57,7 @@ export default async function MainSection1() {
                   </h1>
 
                   <h4 className="scroll-m-20 text-sm font-semibold leading-tight text-muted ml-1 mt-2">
-                    어차피 써야하는 개발비라면 스마트하게 AI견적부터 테스크 추적까지,
+                    쇼피파이, 아임웹, React, 앱 개발까지,
                     <br />
                     최소 30% 더 적은 비용으로 외주를 진행하세요.<span className="text-[#e64646] font-black">*</span>
                   </h4>
