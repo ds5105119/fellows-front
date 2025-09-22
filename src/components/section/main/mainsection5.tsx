bb9ff84484d51d3f8b0e469d97f956f92e1d36b3
김동현 <52624315+ds5105119@users.noreply.github.com>
2025-09-22 04:26:06 +0000

    애니메이션 효과를 추가합니다
    스크롤 시 나타나는 애니메이션 효과를 메인 섹션 5에 추가하여 사용자 경험을 개선합니다.
    
    `components.json`에서 `iconLibrary` 위치를 수정했습니다.

--- src/components/section/main/mainsection5.tsx
+++ src/components/section/main/mainsection5.tsx
@@ -1,5 +1,6 @@
-import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { InView } from "@/components/ui/in-view";
 
 export default async function MainSection5() {
   return (
@@ -17,63 +18,90 @@ export default async function MainSection5() {
 
       <div className="relative grid grid-cols-1 lg:grid-cols-3 gap-8 md:gap-10">
         <div className="col-span-1 bg-muted aspect-square rounded-3xl">
-          <div className="w-full h-full px-6 md:px-10 pt-6 md:pt-10 flex flex-col justify-between overflow-hidden relative">
-            <div className="flex flex-col space-y-1.5 z-50 w-full shrink-0">
-              <div className="flex flex-col space-y-2">
-                <p className="text-xl md:text-2xl font-extrabold tracking-normal text-black leading-normal">
-                  Fellows를 통해
-                  <br />
-                  원스톱으로 견적에서 계약까지
-                </p>
+          <InView
+            variants={{
+              hidden: { opacity: 0, y: 100, filter: "blur(4px)" },
+              visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+            }}
+            viewOptions={{ margin: "0px 0px 100px 0px" }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            <div className="w-full h-full px-6 md:px-10 pt-6 md:pt-10 flex flex-col justify-between overflow-hidden relative">
+              <div className="flex flex-col space-y-1.5 z-50 w-full shrink-0">
+                <div className="flex flex-col space-y-2">
+                  <p className="text-xl md:text-2xl font-extrabold tracking-normal text-black leading-normal">
+                    Fellows를 통해
+                    <br />
+                    원스톱으로 견적에서 계약까지
+                  </p>
+                </div>
               </div>
-            </div>
 
-            <div className="pt-3 md:pt-5 grow w-full relative flex items-end justify-center">
-              <div className="w-full h-fit">
-                <AspectRatio ratio={600 / 448}>
-                  <Image src="/section-5-1.png" alt="메인 섹션 5 이미지" fill className="object-contain" />
-                </AspectRatio>
+              <div className="pt-3 md:pt-5 grow w-full relative flex items-end justify-center">
+                <div className="w-full h-fit">
+                  <AspectRatio ratio={600 / 448}>
+                    <Image src="/section-5-1.png" alt="메인 섹션 5 이미지" fill className="object-contain" />
+                  </AspectRatio>
+                </div>
               </div>
             </div>
-          </div>
+          </InView>
         </div>
         <div className="col-span-1 bg-muted aspect-square rounded-3xl">
-          <div className="w-full h-full px-6 md:px-10 pt-6 md:pt-10 flex flex-col justify-between overflow-hidden relative">
-            <div className="flex flex-col space-y-1.5 z-50 w-full shrink-0">
-              <div className="flex flex-col space-y-2">
-                <p className="text-xl md:text-2xl font-extrabold tracking-normal text-black leading-normal">디자인 및 개발 착수 </p>
+          <InView
+            variants={{
+              hidden: { opacity: 0, y: 100, filter: "blur(4px)" },
+              visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+            }}
+            viewOptions={{ margin: "0px 0px 100px 0px" }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            <div className="w-full h-full px-6 md:px-10 pt-6 md:pt-10 flex flex-col justify-between overflow-hidden relative">
+              <div className="flex flex-col space-y-1.5 z-50 w-full shrink-0">
+                <div className="flex flex-col space-y-2">
+                  <p className="text-xl md:text-2xl font-extrabold tracking-normal text-black leading-normal">디자인 및 개발 착수 </p>
+                </div>
               </div>
-            </div>
 
-            <div className="pt-3 md:pt-5 grow w-full relative flex items-end justify-center">
-              <div className="w-full h-fit">
-                <AspectRatio ratio={600 / 490}>
-                  <Image src="/section-5-2.png" alt="메인 섹션 5 이미지" fill className="object-contain" />
-                </AspectRatio>
+              <div className="pt-3 md:pt-5 grow w-full relative flex items-end justify-center">
+                <div className="w-full h-fit">
+                  <AspectRatio ratio={600 / 490}>
+                    <Image src="/section-5-2.png" alt="메인 섹션 5 이미지" fill className="object-contain" />
+                  </AspectRatio>
+                </div>
               </div>
             </div>
-          </div>
+          </InView>
         </div>
         <div className="col-span-1 bg-muted aspect-square rounded-3xl">
-          <div className="w-full h-full px-6 md:px-10 pt-6 md:pt-10 flex flex-col justify-between overflow-hidden relative">
-            <div className="flex flex-col space-y-1.5 z-50 w-full shrink-0">
-              <div className="flex flex-col space-y-2">
-                <p className="text-xl md:text-2xl font-extrabold tracking-normal text-black leading-normal">
-                  퍼블리싱 및 유지보수까지,
-                  <br />
-                  Fellows가 든든하게 책임집니다
-                </p>
+          <InView
+            variants={{
+              hidden: { opacity: 0, y: 100, filter: "blur(4px)" },
+              visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+            }}
+            viewOptions={{ margin: "0px 0px 100px 0px" }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            <div className="w-full h-full px-6 md:px-10 pt-6 md:pt-10 flex flex-col justify-between overflow-hidden relative">
+              <div className="flex flex-col space-y-1.5 z-50 w-full shrink-0">
+                <div className="flex flex-col space-y-2">
+                  <p className="text-xl md:text-2xl font-extrabold tracking-normal text-black leading-normal">
+                    퍼블리싱 및 유지보수까지,
+                    <br />
+                    Fellows가 든든하게 책임집니다
+                  </p>
+                </div>
               </div>
-            </div>
 
-            <div className="pt-3 md:pt-5 grow w-full relative flex items-end justify-center">
-              <div className="w-full h-fit">
-                <AspectRatio ratio={600 / 448}>
-                  <Image src="/section-5-1.png" alt="메인 섹션 5 이미지" fill className="object-contain" />
-                </AspectRatio>
+              <div className="pt-3 md:pt-5 grow w-full relative flex items-end justify-center">
+                <div className="w-full h-fit">
+                  <AspectRatio ratio={600 / 490}>
+                    <Image src="/section-5-2.png" alt="메인 섹션 5 이미지" fill className="object-contain" />
+                  </AspectRatio>
+                </div>
               </div>
             </div>
-          </div>
+          </InView>
         </div>
       </div>
     </div>
