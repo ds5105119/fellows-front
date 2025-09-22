18fe027e94c95a6a303c4b09b4aa8c1e37938f06
김동현 <52624315+ds5105119@users.noreply.github.com>
2025-09-22 06:09:19 +0000

    메인 섹션 문구를 개선하고 이미지 업데이트
    메인 섹션 3과 5의 문구를 간결하게 수정하고, 강조 스타일을 변경하여 가독성을 높입니다. 또한, 섹션 5의 이미지를 업데이트하여 시각적인 효과를 개선합니다.

--- src/components/section/main/mainsection5.tsx
+++ src/components/section/main/mainsection5.tsx
@@ -97,7 +97,7 @@ export default async function MainSection5() {
               <div className="pt-3 md:pt-5 grow w-full relative flex items-end justify-center">
                 <div className="w-full h-fit">
                   <AspectRatio ratio={600 / 490}>
-                    <Image src="/section-5-2.png" alt="메인 섹션 5 이미지" fill className="object-contain" />
+                    <Image src="/section-5-3.png" alt="메인 섹션 5 이미지" fill className="object-contain" />
                   </AspectRatio>
                 </div>
               </div>
